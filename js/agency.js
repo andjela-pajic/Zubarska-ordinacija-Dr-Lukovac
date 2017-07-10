ae8761c5d8b1920ae85a7bc2d0a796a9cec96635
andjela-pajic <andjela.pajic90@gmail.com>
2017-07-11 01:30:14 +0200

    commit latest changes

--- js/agency.js
+++ js/agency.js
@@ -7,7 +7,7 @@
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 65)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -15,19 +15,19 @@
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
-        offset: 51
+        offset: 70
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
 
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
-            top: 100
+            top: 70
         }
-    })
+    });
 
 })(jQuery); // End of use strict
